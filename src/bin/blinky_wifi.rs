0f4459b6493d77c9b49f27d8577d9ad8fd11d571
zacharytam <78257053+zacharytam@users.noreply.github.com>
2025-11-12 20:25:33 +0800

    Update blinky_wifi.rs

--- src/bin/blinky_wifi.rs
+++ src/bin/blinky_wifi.rs
@@ -42,8 +42,8 @@ async fn cyw43_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'stat
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let fw = include_bytes!("../../../../cyw43-firmware/43439A0.bin");
-    let clm = include_bytes!("../../../../cyw43-firmware/43439A0_clm.bin");
+    let fw = include_bytes!("cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("cyw43-firmware/43439A0_clm.bin");
 
     // To make flashing faster for development, you may want to flash the firmwares independently
     // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
